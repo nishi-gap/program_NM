4d98a333569d88831e0d3116f368938afb02741b
nishi-gap <100747798+nishi-gap@users.noreply.github.com>
2022-03-24 18:49:01 +0900

    動くし初期値の値よりは目的関数の値が小さくはなるけど、遅いし結果がよくない

--- src/pylib/setRuling.py
+++ src/pylib/setRuling.py
@@ -1,14 +1,15 @@
 import numpy as np
 from scipy.optimize import minimize, LinearConstraint
+from scipy.misc import derivative
 
 from src.pylib import DevSrf
 
-##標準偏差により評価
-##各ベクトルのとる範囲が-1から1のため十分小さくなれば
+eps = 1e-2 #仮置き
+eps_list = [eps, -eps,]
+
 def getSD(p: np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, dx:float):
     f = 0.0
     for i in range(ds.rulingNum + 1):
-        slicedImage = np.empty((3,),dtype=float)
         r = g = b = 0.0
         cnt = 0
         if i == 0:                
@@ -61,28 +62,48 @@ def getSD(p: np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, dx:float):
                  round(y * (p[i] - p[i + ds.rulingNum])/ ds.modelHeight + p[i] * dx)):
                     if(0 < x and x < ds.MapWidth):
                         f += 1 - np.dot(Vave, img[x][y])
-    print("f",f)
     return f
 
-eps = 1e-2 #仮置き
+def getSD2(p:np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, dx:float, i:int):
+    f = 0.0
+    return f
+
+def cb_getSD(p: np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, dx:float, i:int):
+    if i != -1:
+        return  getSD2(p,ds,img,dx,i)
+    else:
+        return getSD(p,ds,img,dx)
+
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.misc.derivative.html
+#https://github.com/tttamaki/lecture_code/blob/main/MachineLearningMath/differentiation_scipy.ipynb
+#https://home.hirosaki-u.ac.jp/jupyter/python-sk/
+#x:微分するパラメータ, eps:微小区間, ndim:何階微分
+def grad(x:float, ndim:int, p: np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, dx:float):
+    f = derivative(func = getSD, x0 = x, args = (p,ds,img,dx), dx = eps, n = ndim)
+    return f
+
 def Func_Der(p:np.ndarray, ds: DevSrf.DevSrf,img:np.array, dx:float):
+    print("called First derivative")
     f_der = np.zeros(p.size)
-    x = p
-    for i in range(p.size):
-        x[i] += eps
-        f1 = getSD(x,ds,img,dx)
-        x[i] -= 2*eps
-        f2 = getSD(x,ds,img,dx)
-        x[i] += eps
-        f_der[i] = (f1 - f2)/(2 * eps)
+    if 0:
+        #scipyの数値微分を使ったversion
+        for i in range(p.size):
+            f_der[i] = grad(p[i], 1, p, ds, img, dx)
+        return f_der
+    else:
+        x = p
+        for i in range(p.size):
+            x[i] += eps
+            f1 = getSD(x,ds,img,dx)
+            x[i] -= 2*eps
+            f2 = getSD(x,ds,img,dx)
+            x[i] += eps
+            f_der[i] = (f1 - f2)/(2 * eps)
 
     return f_der
 
-"""
-f1 = f(i+h,j+h), f2 = f(i+h,j-h), f3 = f(i-h,j+h), f4 = f(i-h,j+h)
-df/dxdy = (f1 + f3 - f4 - f2)/h^2
-"""
-eps_list = [eps, -eps,]
+#f1 = f(i+h,j+h), f2 = f(i+h,j-h), f3 = f(i-h,j+h), f4 = f(i-h,j+h)
+#df/dxdy = (f1 + f3 - f4 - f2)/h^2
 def diff(i:int, j:int, p:np.ndarray, ds:DevSrf.DevSrf, img:np.array, dx:float):
     f = np.zeros(4)
     for n in range(2):
@@ -92,18 +113,26 @@ def diff(i:int, j:int, p:np.ndarray, ds:DevSrf.DevSrf, img:np.array, dx:float):
             f[2 * n + m] = getSD(p,ds,img,dx)
     return (f[0] - f[2] + f[3] - f[1])/(eps * eps)
 
+
+#ヘッセ行列の2階微分のやり方間違っているかも
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.HessianUpdateStrategy.html#scipy.optimize.HessianUpdateStrategy
+#scipy.optimize.HessianUpdateStrategy　使えないか試してみる
 def Func_Hess(p:np.ndarray, ds: DevSrf.DevSrf,img:np.array, dx:float):
+    print("called Hessian")
     H = np.zeros((p.size, p.size, ))
     for i in range(p.size):
-        for j in range(p.size):
+        for j in range(i, p.size):
             H[i][j] = diff(i, j, p, ds, img, dx)
+            H[j][i] = H[i][j] #ヘッセ行列は対角行列であるため
     return H
 
 #scipyによる最適化
 #https://scipy.github.io/devdocs/tutorial/optimize.html
 #https://docs.scipy.org/doc/scipy/tutorial/optimize.html#constrained-minimization-of-multivariate-scalar-functions-minimize
 def optimization(x:np.ndarray, ds: DevSrf.DevSrf,img:np.array, dx:float):
-    return getSD(x,ds,img, dx)
+    f = getSD(x,ds,img, dx)
+    print("called optimization ", f)
+    return f
 
 #n　rulingの数
 #A　パラメータの関係を表す行列(m * n) mは制約の数？->　2 * (n - 1)
@@ -126,6 +155,17 @@ def setLinearConstrait(n:int):
 
     return A, lb, lu
 
+
+def cb_optimization(x:np.ndarray, state):
+
+    print("callback")
+    print("------------------")
+    print("x : ", x)
+    print("iteration : ", state.nit)
+    print("state x :", state.x)
+    print("------------------")
+
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
 def setRuling(ds:DevSrf.DevSrf, img: np.array):  
     dx = ds.MapWidth/ds.modelWidth
     step = ds.modelWidth/(ds.rulingNum + 1)
@@ -137,6 +177,10 @@ def setRuling(ds:DevSrf.DevSrf, img: np.array):
 
     #最適化 <-パラメータの与え方と制約を与えればとりあえずは動くはず
     res = minimize(optimization, p, args = (ds,img, dx), method='trust-constr', 
-    jac=Func_Der, hess=Func_Hess, constraints = linearConstrait, options={'gtol': 1e-2, 'disp': True})
-    
+    jac=Func_Der, hess=Func_Hess, constraints = linearConstrait, callback= cb_optimization, options={'gtol': 1e-2, 'disp': True})
+
+    print("result")
+    print("Is success : ", res.key)
+    print("parameter : ", res.x)
+
     return res

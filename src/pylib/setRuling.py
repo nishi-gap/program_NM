e8df159203e4554b837643b550a2f29c778e7199
nishi-gap <100747798+nishi-gap@users.noreply.github.com>
2022-04-10 17:09:43 +0900

    パラメータの位置がおかしいため制約の与え方を修正する必要ある

--- src/pylib/setRuling.py
+++ src/pylib/setRuling.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import minimize, LinearConstraint
+from scipy.optimize import minimize, Bounds
 from numpy import linalg as LA
 from src.pylib import DevSrf
 from src.pylib import DrawRuling
@@ -12,6 +12,7 @@ ystep = 0.1
 
 IS_DEBUG_MODE = 0
 
+#正則化を外してみている
 def Smoothing(img:np.ndarray,x:float,y:float, X:int,Y:int, ratio:list):
     xi = int(x * ratio[0])
     yi = int(y * ratio[1])
@@ -22,9 +23,9 @@ def Smoothing(img:np.ndarray,x:float,y:float, X:int,Y:int, ratio:list):
         Ism += xf * (img[xi + int(ratio[0] * xstep)][yi] - img[xi][yi])
         if yi + int(ratio[1] * ystep) < Y:
             Ism += yf * (img[xi][yi + int(ratio[1] * ystep)] - img[xi][yi])
-    Ism = 2 * Ism - 1
-    Ism /= LA.norm(Ism)
-    Ism = (Ism + 1)/2
+    #Ism = 2 * Ism - 1
+    #Ism /= LA.norm(Ism)
+    #Ism = (Ism + 1)/2
     return Ism
 
 def C2V(img:np.ndarray):
@@ -42,7 +43,9 @@ def getSD(p: np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, ratio:list):
         if i == 0: 
             while y < yt:
                 x = 0
-                xr = min(y * (p[i + 1] - p[i + ds.rulingNum + 1])/ ds.modelHeight + p[i + 1], ds.modelWidth)   
+                pr1 = np.array([p[i + 1], p[i + 2 * ds.rulingNum + 1]])
+                pr2 = np.array([p[i + ds.rulingNum + 1], p[i + 3 * ds.rulingNum + 1]])
+                xr = min((y - pr2[1]) * (pr1[0] - pr2[0])/ (pr1[1] - pr2[1]) + pr2[0], ds.modelWidth) 
                 while x < xr:
                     im = (Smoothing(img,x,y,ds.MapWidth,ds.MapHeight,ratio))
                     if(len(slicedImage) == 0):
@@ -53,7 +56,9 @@ def getSD(p: np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, ratio:list):
                 y += ystep
         elif i == ds.rulingNum:
             while y < yt:
-                x = max(0,y * (p[i - 1] - p[i + ds.rulingNum - 1])/ ds.modelHeight + p[i - 1])
+                pl1 = np.array([p[i - 1], p[i + 2 * ds.rulingNum - 1]])
+                pl2 = np.array([p[i + ds.rulingNum - 1], p[i + 3 * ds.rulingNum - 1]])
+                x = max(0,(y - pl2[1]) * (pl1[0] - pl2[0])/ (pl1[1] - pl2[1]) + pl2[0])
                 xr = ds.modelWidth
                 while x < xr:
                     im = (Smoothing(img,x,y,ds.MapWidth,ds.MapHeight,ratio))
@@ -65,8 +70,13 @@ def getSD(p: np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, ratio:list):
                 y += ystep
         else:
             while y < yt:
-                x = max(0, y * (p[i - 1] - p[i + ds.rulingNum - 1])/ds.modelHeight + p[i + ds.rulingNum - 1])
-                xr = min(y * (p[i] - p[i + ds.rulingNum])/ds.modelHeight + p[i],ds.modelWidth)
+                pl1 = np.array([p[i - 1], p[i + 2 * ds.rulingNum - 1]])
+                pl2 = np.array([p[i + ds.rulingNum - 1], p[i + 3 * ds.rulingNum - 1]])
+                x = min(max(0,(y - pl2[1]) * (pl1[0] - pl2[0])/(pl1[1] - pl2[1]) + pl2[0]),ds.modelWidth)
+
+                pr1 = np.array([p[i], p[i + 2 * ds.rulingNum]])
+                pr2 = np.array([p[i + ds.rulingNum], p[i + 3 * ds.rulingNum]])
+                xr = min(max(0,(y - pr2[1]) * (pr1[0] - pr2[1])/(pr1[1] - pr2[1]) + pr2[0],0),ds.modelWidth)
                 while x < xr:
                     im = (Smoothing(img,x,y,ds.MapWidth,ds.MapHeight,ratio))
                     if(len(slicedImage) == 0):
@@ -93,7 +103,9 @@ def getSD2(p:np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, ratio:list, n:list):
         if i == 0: 
             while y < yt:
                 x = 0
-                xr = min(y * (p[i + 1] - p[i + ds.rulingNum + 1])/ ds.modelHeight + p[i + 1], ds.modelWidth)   
+                pr1 = np.array([p[i + 1], p[i + 2 * ds.rulingNum + 1]])
+                pr2 = np.array([p[i + ds.rulingNum + 1], p[i + 3 * ds.rulingNum + 1]])
+                xr = min((y - pr2[1]) * (pr1[0] - pr2[0])/ (pr1[1] - pr2[1]) + pr2[0], ds.modelWidth)   
                 while x < xr:
                     im = (Smoothing(img,x,y,ds.MapWidth,ds.MapHeight, ratio))
                     if(len(slicedImage) == 0):
@@ -102,10 +114,11 @@ def getSD2(p:np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, ratio:list, n:list):
                         slicedImage = np.vstack([slicedImage,im])
                     x += xstep
                 y += ystep
-
         elif i == ds.rulingNum:
             while y < yt:
-                x = max(0,y * (p[i - 1] - p[i + ds.rulingNum - 1])/ ds.modelHeight + p[i - 1])
+                pl1 = np.array([p[i - 1], p[i + 2 * ds.rulingNum - 1]])
+                pl2 = np.array([p[i + ds.rulingNum - 1], p[i + 3 * ds.rulingNum - 1]])
+                x = max(0,(y - pl2[1]) * (pl1[0] - pl2[0])/ (pl1[1] - pl2[1]) + pl2[0])
                 xr = ds.modelWidth
                 while x < xr:
                     im = (Smoothing(img,x,y,ds.MapWidth,ds.MapHeight, ratio))
@@ -115,11 +128,15 @@ def getSD2(p:np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, ratio:list, n:list):
                         slicedImage = np.vstack([slicedImage,im])
                     x += xstep
                 y += ystep
-
         else:
             while y < yt:
-                x = min(max(0,y * (p[i - 1] - p[i + ds.rulingNum - 1])/ds.modelHeight + p[i + ds.rulingNum - 1]),ds.modelWidth)
-                xr = min(max(0,y * (p[i] - p[i + ds.rulingNum])/ds.modelHeight + p[i],0),ds.modelWidth)
+                pl1 = np.array([p[i - 1], p[i + 2 * ds.rulingNum - 1]])
+                pl2 = np.array([p[i + ds.rulingNum - 1], p[i + 3 * ds.rulingNum - 1]])
+                x = min(max(0,(y - pl2[1]) * (pl1[0] - pl2[0])/(pl1[1] - pl2[1]) + pl2[0]),ds.modelWidth)
+
+                pr1 = np.array([p[i], p[i + 2 * ds.rulingNum]])
+                pr2 = np.array([p[i + ds.rulingNum], p[i + 3 * ds.rulingNum]])
+                xr = min(max(0,(y - pr2[1]) * (pr1[0] - pr2[1])/(pr1[1] - pr2[1]) + pr2[0],0),ds.modelWidth)
                 if x > xr: x, xr = xr, x
                 while x < xr:
                     im = (Smoothing(img,x,y,ds.MapWidth,ds.MapHeight,ratio))
@@ -132,7 +149,6 @@ def getSD2(p:np.ndarray, ds:DevSrf.DevSrf, img:np.ndarray, ratio:list, n:list):
                         
         Vave = slicedImage.mean(axis = 0)
         f1 = 0.0
-
         for v in slicedImage:
             f1 += LA.norm(v - Vave)
         f += f1
@@ -165,6 +181,7 @@ def optimization(x:np.ndarray, ds: DevSrf.DevSrf,img:np.array, ratio:list):
 #https://www.delftstack.com/ja/howto/matplotlib/how-to-automate-plot-updates-in-matplotlib/
 #matplotlibを使ってのグラフ更新
 IterCnt = 0
+rulingNum = 0
 RuledLines = np.empty(0,dtype=float)
 def cb_optimization(x:np.ndarray):
     global IterCnt, RuledLines
@@ -173,9 +190,9 @@ def cb_optimization(x:np.ndarray):
     print("------------------")
     print("iteration : ", IterCnt)
     print("parametor x :")
-    for i in range(int(x.size/2)):
+    for i in range(rulingNum):
         #r.append([[x[i], x[i + int(x.size/2)]],[x[i + int(x.size/4)], x[i + 3 * int(x.size/4)]]])  
-        print(i, " :  xL{%f}, xR{%f}" %(x[i], x[i + int(x.size/2)]))
+        print(i, " :  xL{%f, %f}, xR{%f, %f}" %(x[i], x[i + 2 * rulingNum], x[i + rulingNum], x[i + 3 * rulingNum]))
     print("------------------")
     if(len(RuledLines) == 0):
         RuledLines = np.hstack([RuledLines,x])
@@ -183,18 +200,20 @@ def cb_optimization(x:np.ndarray):
 
 def setCons(p:np.ndarray, ds:DevSrf.DevSrf):
     cons = ()
-    if ds.rulingNum != 1:
-        for j in range(2):
-            for i in range(ds.rulingNum - 1):
-                cons = cons + ({'type':'ineq', 'fun' : lambda p, n = i + j * ds.rulingNum: -(p[n] - p[n + 1])},)
-    for i in range(2 * ds.rulingNum):
-        cons = cons + ({'type':'eq', 'fun' : lambda p, n = i: (p[n] - 0) * (p[n] - ds.modelWidth) * 
-        (p[n + 2 * ds.rulingNum] - 0) * (p[n + 2 * ds.rulingNum] - ds.modelHeight)},)
+    for j in range(2):
+        for i in range(ds.rulingNum - 1):
+            cons = cons + ({'type':'ineq', 'fun' : lambda p, n = i + j * ds.rulingNum: -(p[n] - p[n + 1])},)
+    #領域内に点が乗るように制約を与える
+    for j in range(2):
+        for i in range(ds.rulingNum):
+            cons = cons + ({'type':'eq', 'fun' : lambda p, k = j, n = i + j * ds.rulingNum: (p[n] - 0) * (p[n] - ds.modelWidth) * 
+            (p[n + 2 * ds.rulingNum] - k * ds.modelHeight)},)
     return cons
 
 #https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
 def setRuling(ds:DevSrf.DevSrf, img: np.array):  
-
+    global rulingNum
+    rulingNum = ds.rulingNum
     dx = ds.MapWidth/ds.modelWidth
     dy = ds.MapHeight/ds.modelHeight
     ratio = [dx,dy]
@@ -208,26 +227,31 @@ def setRuling(ds:DevSrf.DevSrf, img: np.array):
     #SLSQPの制約の与え方
     #https://towardsdatascience.com/optimization-with-scipy-and-application-ideas-to-machine-learning-81d39c7938b8
     #https://teratail.com/questions/181787
+    #https://docs.scipy.org/doc/scipy/tutorial/optimize.html#sequential-least-squares-programming-slsqp-algorithm-method-slsqp
     #パラメータが変わったためここも修正必須
-    bnds = ()
-    for i in range(2):
-        for j in range(ds.rulingNum):
-            if i < 2:
-                bnds = bnds + ((0,ds.modelWidth),)
-            else:
-                bnds = bnds + ((0,ds.modelHeight),)
+    cons = ()
+    #交差判定
+    for j in range(2):
+        for i in range(ds.rulingNum - 1):
+            cons = cons + ({'type':'ineq', 'fun' : lambda p, n = i + j * ds.rulingNum: (p[n + 1] - p[n] - eps)},)
+    #領域内に点が乗るように制約を与える
+    for j in range(2):
+        for i in range(ds.rulingNum):
+            cons = cons + ({'type':'eq', 'fun' : lambda p, k = j, n = i + j * ds.rulingNum: (p[n] - 0) * (p[n] - ds.modelWidth) * 
+            (p[n + 2 * ds.rulingNum] - k * ds.modelHeight)},)
 
+    lb = np.zeros(4 * ds.rulingNum)
+    ub = np.full(4 * ds.rulingNum, ds.modelHeight)
+    for i in range(2 * ds.rulingNum): 
+        ub[i] = (ds.modelWidth)
+    bnds = Bounds(lb, ub)
     maxiter = 20
     res = minimize(optimization, x0 = p, args = (ds, img, ratio), method = 'SLSQP', jac = Func_Der, 
-    constraints = setCons(p, ds), bounds = bnds, callback = cb_optimization,
+     constraints= cons, bounds = bnds, callback = cb_optimization,
      options = {'gtol':1e-1, 'disp':True, 'eps':eps, 'maxiter':maxiter})
     
     #print(res)
     for i in range(ds.rulingNum):
-        print(i, " :  xL{%f}, xR{%f}" %(res.x[i], res.x[i + ds.rulingNum]))
-    print("===========================")
-
-    
+        print(i, " :  xL{%f, %f}, xR{%f, %f}" %(res.x[i], res.x[i + 2 * ds.rulingNum], res.x[i + ds.rulingNum], res.x[i + 3 * ds.rulingNum]))
+    print("===========================")   
     DrawRuling.dispResult(RuledLines)
-    return RuledLines
-     #最適化で折り線がどう動いているか見れるようにする
